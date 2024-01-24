620d17e798c32006af01b819de9837584ceaf5c9
Oleksii Dovhaniuk <alexdovhaniuk@gmail.com>
2024-01-24 18:35:05 +0200

    2024-01-24 now it is more chanses that the Gale Shapley Algorithm works

--- src/service/EditBasicGaleShapleyAlgorithmService.R
+++ src/service/EditBasicGaleShapleyAlgorithmService.R
@@ -27,9 +27,9 @@ is_better_option <- function(woman, current_man, women_matched, women_preference
   engaged_man <- women_matched[woman]
   engaged_man_rank <- which(women_preferences[woman, ] == engaged_man)
   current_man_rank <- which(women_preferences[woman, ] == current_man)
-  
+  print(paste0('COMPARE: ', current_man, '[', current_man_rank, '] V ', engaged_man, '[', engaged_man_rank, ']'))
   if (current_man_rank < engaged_man_rank) {
-    return(TRUE)
+    return(engaged_man)
   }
     return(FALSE)
 }
@@ -53,12 +53,14 @@ gale_shapley <- function(men_preferences, women_preferences) {
     current_man <- which( is.na(men_matched) )[1]
     
     # Find the woman he prefers the most among those he hasn't proposed to
-    preferred_woman <- which( is.na(proposed[current_man, ]) )[1]
-    preferred_woman <- ifelse(is.na(preferred_woman), 1, preferred_woman)
-    proposed[current_man, preferred_woman] <- TRUE
+    preferred_woman_index <- which( is.na(proposed[current_man, ]) )[1]
+    preferred_woman_index <- ifelse(is.na(preferred_woman_index), 1, preferred_woman_index)
+    preferred_woman <- men_preferences[current_man, preferred_woman_index]
+    proposed[current_man, preferred_woman_index] <- TRUE
     
     print(paste0('match: ', current_man, '-', preferred_woman))
-    print(men_matched)
+    print(paste0('m: ', men_matched))
+    print(paste0('w: ', women_matched))
     print(proposed)
     print(paste0('counter: ', counter))
     
@@ -68,23 +70,26 @@ gale_shapley <- function(men_preferences, women_preferences) {
       next
     }
     
-    if ( is_better_option(preferred_woman, current_man, women_matched, women_preferences) ) {
+    better_option <- is_better_option(preferred_woman, current_man, women_matched, women_preferences)
+    if ( better_option != FALSE ) {
       women_matched[preferred_woman] <- current_man
       men_matched[current_man] <- preferred_woman
+      men_matched[better_option] <- NA
       next
     }
     
-    if (counter > n*n){
+    if (counter == n*n) {
       break
     }
+    
   }
   # Return the final matching
   return(men_matched)
 }
 
 # Example usage
-men_preferences <- matrix(c(1, 2, 3, 1, 2, 3, 1, 2, 3), nrow = 3, byrow = TRUE)
-women_preferences <- matrix(c(1, 2, 3, 1, 2, 3, 1, 2, 3), nrow = 3, byrow = TRUE)
+men_preferences <- matrix(c(2, 3, 1, 1, 3, 2, 3, 2, 1), nrow = 3, byrow = TRUE)
+women_preferences <- matrix(c(1, 2, 3, 2, 3, 1, 1, 2, 3), nrow = 3, byrow = TRUE)
 
 result <- gale_shapley(men_preferences, women_preferences)
 print(result)
